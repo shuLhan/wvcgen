4ff79ceddcf547da97e1c427a6366e280e712295
Mhd Sulhan <ms@kilabit.info>
2016-02-12 00:18:49 +0700

    Add feature to count comment length, NOT including header "/*...*/".

---
+++ feature/commentlen.go
@@ -0,0 +1,54 @@
+// Copyright 2016 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package feature
+
+import (
+	"github.com/shuLhan/dsv"
+	"github.com/shuLhan/tekstus"
+)
+
+// CommentLen feature for compute the length of edit comment.
+type CommentLen struct {
+	dsv.Column
+}
+
+func init() {
+	commentLen := CommentLen{
+		Column: dsv.Column{
+			Type: dsv.TInteger,
+			Name: "commentlen",
+		},
+	}
+
+	// Register to list of feature
+	ListFeatureAdd(&commentLen)
+}
+
+/*
+GetValues return feature values.
+*/
+func (ftr *CommentLen) GetValues() dsv.Column {
+	return ftr.Column
+}
+
+// Compute will count number of bytes that is used in comment, NOT including
+// the header content "/* ... */".
+func (ftr *CommentLen) Compute(dataset dsv.Dataset) {
+	col := dataset.GetColumnByName("editcomment")
+	leftcap := []byte("/*")
+	rightcap := []byte("*/")
+
+	for _, rec := range col.Records {
+		cmt := rec.ToByte()
+
+		cmt, _ = tekstus.EncapsulateTrim(cmt, leftcap, rightcap)
+
+		r := &dsv.Record{
+			V: int64(len(cmt)),
+		}
+
+		ftr.PushBack(r)
+	}
+}
